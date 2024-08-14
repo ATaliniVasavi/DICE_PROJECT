397359f5a0a011f53b51bf48164ec43a010c37b2
vasavi <amaratalini@gmail.com>
2024-08-14 11:14:05 +0530

    Initial commit

---
+++ Dice Project/dist/server.dev.js
@@ -0,0 +1,19 @@
+
+
+var express = require("express");
+
+var app = express();
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + "/index.html"); // response.send("<h1>Vasavi</h1>");
+});
+app.listen(3000, function () {
+  console.log("Server Initiated !!");
+});
+app.get('/style.css', function (req, res) {
+  res.sendFile(__dirname + "/" + "style.css");
+});
+app.get('/script.js', function (req, res) {
+  res.sendFile(__dirname + "/" + "script.js");
+});
+app.use(express["static"]('public'));
+app.use('/images', express["static"]('images'));
